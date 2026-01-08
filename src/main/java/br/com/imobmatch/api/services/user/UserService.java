293cffdc817e8e178c2019d625925ffdd5fb6321
Ruanrodrigues20 <ruan.rodrigues.silvaa@gmail.com>
2026-01-08 10:37:30 -0300

    feat(user): expose entity lookup for auth token refresh

--- src/main/java/br/com/imobmatch/api/services/user/UserService.java
+++ src/main/java/br/com/imobmatch/api/services/user/UserService.java
@@ -1,8 +1,10 @@
 package br.com.imobmatch.api.services.user;
 
+import br.com.imobmatch.api.dtos.phone.PhonePostDTO;
+import br.com.imobmatch.api.dtos.phone.PhoneResponseDTO;
 import br.com.imobmatch.api.dtos.user.UserResponseDTO;
-import br.com.imobmatch.api.exceptions.user.UserExists;
-import br.com.imobmatch.api.exceptions.user.UserNotFound;
+import br.com.imobmatch.api.exceptions.user.UserExistsException;
+import br.com.imobmatch.api.exceptions.user.UserNotFoundException;
 import br.com.imobmatch.api.models.user.User;
 import br.com.imobmatch.api.models.user.UserRole;
 
@@ -10,7 +12,11 @@ import java.util.UUID;
 
 public interface UserService {
 
-    UserResponseDTO create(String email, String password, UserRole role) throws UserExists;
-    UserResponseDTO getById(UUID id) throws UserNotFound;
-    UserResponseDTO getByEmail(String email) throws UserNotFound;
+    UserResponseDTO create(String email, String password, UserRole role) throws UserExistsException;
+    UserResponseDTO getById(UUID id) throws UserNotFoundException;
+    UserResponseDTO getByEmail(String email) throws UserNotFoundException;
+    PhoneResponseDTO addPhone(PhonePostDTO phonePostDTO, UUID id) throws UserNotFoundException;
+    User findEntityById(UUID id) throws UserNotFoundException;
+
+    UserResponseDTO deleteById(UUID id);
 }
--- src/main/java/br/com/imobmatch/api/services/user/UserServiceImpl.java
+++ src/main/java/br/com/imobmatch/api/services/user/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package br.com.imobmatch.api.services.user;
 
+import br.com.imobmatch.api.dtos.phone.PhonePostDTO;
+import br.com.imobmatch.api.dtos.phone.PhoneResponseDTO;
 import br.com.imobmatch.api.dtos.user.UserResponseDTO;
-import br.com.imobmatch.api.exceptions.user.UserExists;
-import br.com.imobmatch.api.exceptions.user.UserNotFound;
+import br.com.imobmatch.api.exceptions.user.UserExistsException;
+import br.com.imobmatch.api.exceptions.user.UserNotFoundException;
 import br.com.imobmatch.api.models.user.User;
 import br.com.imobmatch.api.models.user.UserRole;
 import br.com.imobmatch.api.repositories.UserRepository;
@@ -22,7 +24,7 @@ public class UserServiceImpl implements UserService {
 
     public UserResponseDTO create(String email, String password, UserRole role) {
         if(this.userRepository.findByEmail(email).isPresent()){
-            throw new UserExists();
+            throw new UserExistsException();
         }
 
         String encryptedPassword = this.bCryptPasswordEncoder.encode(password);
@@ -50,16 +52,41 @@ public class UserServiceImpl implements UserService {
             userResponseDTO.setRole(optionalUser.get().getRole());
             return userResponseDTO;
         }
-        throw new UserNotFound();
+        throw new UserNotFoundException();
     }
 
     public UserResponseDTO getByEmail(String email) {
         User user = userRepository.findByEmail(email)
-                .orElseThrow(UserNotFound::new);
+                .orElseThrow(UserNotFoundException::new);
         return new UserResponseDTO(
                 user.getId(),
                 user.getEmail(),
                 user.getRole()
         );
+    }
+
+    @Override
+    public PhoneResponseDTO addPhone(PhonePostDTO phonePostDTO, UUID id) throws UserNotFoundException {
+        return null;
+    }
+
+    @Override
+    public User findEntityById(UUID id) throws UserNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(UserNotFoundException::new);
+        return user;
+    }
+
+    @Override
+    public UserResponseDTO deleteById(UUID id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(UserNotFoundException::new);
+
+        userRepository.delete(user);
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+        userResponseDTO.setId(user.getId());
+        userResponseDTO.setEmail(user.getEmail());
+        userResponseDTO.setRole(user.getRole());
+        return userResponseDTO;
     }
 }
