3a9ad1f1684026adf7a92c270d243bf48646dca3
Everton Daniel <evertonrdaniel@gmail.com>
2026-01-10 14:14:41 -0300

    doc(OwnerService): create a simple javadoc description for OwnerServie

--- src/main/java/br/com/imobmatch/api/services/owner/OwnerService.java
+++ src/main/java/br/com/imobmatch/api/services/owner/OwnerService.java
@@ -11,12 +11,52 @@ import br.com.imobmatch.api.models.owner.Owner;
 import java.util.UUID;
 
 public interface OwnerService {
-
-    OwnerResponseDTO createOwner(OwnerPostDTO owner) throws OwnerExistsException;
-    OwnerResponseDTO updateOwner(OwnerPatchDTO owner );
+    /**
+     * Creates a new  and a new user.
+     * The Owner and User are unique and permanently linked to each other until their complete deletion.
+     *
+     * @param postDto Required data for create owner
+     * @return Informative information's. contains name and id of owner
+     */
+    OwnerResponseDTO createOwner(OwnerPostDTO postDto) throws OwnerExistsException;
+    /**
+     *Update authenticated owner information in the system.
+     *The editable information is name and CPF.
+     *
+     * @param ownerDto New value to be updated. Only valid and present (not null) data will be updated.
+     * @return informative information's. contains name and id of owner.
+     */
+    OwnerResponseDTO updateOwner(OwnerPatchDTO ownerDto );
+    /**
+     * Returns a detailed view of the authenticated owner in the system.
+     * Including their CPF and email address.
+     *
+     * @return DTO containing id, name, cpf, email, role and primary phone of the owner
+     */
     OwnerGetResponseDto getAuthenticatedOwner();
+    /**
+     *Deletes the system owner and the user associated with them.
+     *Requires the user's password to confirm the deletion.
+     *It does not return any values.
+     *
+     * @param passwordConfirm User password.
+     */
     void deleteOwner(PasswordUserDeleteDTO passwordConfirm);
+
+    /**
+     * Retrieves a DTO with more detailed information about the owner and their user.
+     *
+     * @param id userId.
+     * @return ADTO containing owner data.
+     */
     OwnerGetResponseDto getOwnerByid(UUID id);
+
+    /**
+     * Get the owner entity and returns.
+     *
+     * @param id userId.
+     * @return The instance of owner.
+     */
     Owner findEntityById(UUID id);
 
 }
