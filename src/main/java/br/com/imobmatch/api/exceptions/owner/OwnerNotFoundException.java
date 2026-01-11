9296fd71fbe17a974f6463202d5cfa0c4099cb8c
Everton Daniel <evertonrdaniel@gmail.com>
2026-01-11 14:16:17 -0300

    fix: remove trash

--- src/main/java/br/com/imobmatch/api/exceptions/owner/OwnerNotFoundException.java
+++ src/main/java/br/com/imobmatch/api/exceptions/owner/OwnerNotFoundException.java
@@ -2,7 +2,7 @@ package br.com.imobmatch.api.exceptions.owner;
 
 public class OwnerNotFoundException extends RuntimeException {
     public OwnerNotFoundException() {
-        super("The owner associated with the user does not exist");us
+        super("The owner associated with the user does not exist");
 
     }
 }
