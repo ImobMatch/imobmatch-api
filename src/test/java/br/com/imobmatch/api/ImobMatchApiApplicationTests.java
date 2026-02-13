52129ee9e5a45de9c1240e8699c203557f29f275
Ruanrodrigues20 <ruan.rodrigues.silvaa@gmail.com>
2026-02-13 14:11:41 -0300

    refactor test

--- src/test/java/br/com/imobmatch/api/ImobMatchApiApplicationTests.java
+++ src/test/java/br/com/imobmatch/api/ImobMatchApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ImosbMatchApiApplicationTests {
+class ImobMatchApiApplicationTests {
 
 	@Test
 	void contextLoads() {
